00ec6002d9c6a3e6bf58a8c61208fdd766f8a12e
ilya <tojlctbly@gmail.com>
2022-09-02 10:56:06 +0300

    решение задачи 05 07

---
+++ task05/Program.cs
@@ -0,0 +1,14 @@
+﻿// программа которая будет на вход принимает
+// одно число (N), а  на выходе показывает все целые
+//  числа промежутка от -N до N
+//  4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+//  2 -> "-2, -1, 0, 1, 2"
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int count = -num1;
+while (count <= num1){
+    Console.Write($"{count}, ");
+    count = count ++;
+}
