6b19c1901b501713a1bd397f75321ac1c956cf83
ilya <tojlctbly@gmail.com>
2022-09-02 10:21:26 +0300

    решение задачи 03 - сжатие кода

--- task03/Program.cs
+++ task03/Program.cs
@@ -6,36 +6,12 @@
 Console.Clear();
 Console.Write("Введите число от 1 до 7: ");
 string day = Console.ReadLine();
-if (day == "1")
-{
-    Console.Write("понедельник");
-}
-else if (day == "2")
-{
-    Console.Write("вторник");
-}
-else if (day == "3")
-{
-    Console.Write("среда");
-}
-else if (day == "4")
-{
-    Console.Write("четверг");
-}
-else if (day == "5")
-{
-    Console.Write("пятница");
-}
-else if (day == "6")
-{
-    Console.Write("суббота");
-}
-else if (day == "7")
-{
-    Console.Write("воскресенье");
-}
-else
-{
-    Console.Write("введено не верное значение.");
-}
+if (day == "1") Console.Write("понедельник");
+else if (day == "2") Console.Write("вторник");
+else if (day == "3") Console.Write("среда");
+else if (day == "4") Console.Write("четверг");
+else if (day == "5") Console.Write("пятница");
+else if (day == "6") Console.Write("суббота");
+else if (day == "7") Console.Write("воскресенье");
+else Console.Write("Данного дня недели не существует");
 
