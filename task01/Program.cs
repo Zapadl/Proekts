f5330d10d51e1c2f3fc54077d5e77e555a5e4c25
ilya <tojlctbly@gmail.com>
2022-09-02 09:38:27 +0300

    решение задачи 01

--- task01/Program.cs
+++ task01/Program.cs
@@ -3,3 +3,16 @@
 // a=2,b=10-> нет
 // a=9,b=-3-> да
 // a=-3,b=9-> нет
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int square = num2 * num2;
+if (square == num1) {
+    Console.Write($"число {num1} является квадратом {num2}");
+}  
+else {
+    Console.Write($"число {num1} не является квадратом {num2}");
+}
