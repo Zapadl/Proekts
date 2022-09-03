0c1bffcfd1c198239f87e570fb884842119d20c2
ilya <tojlctbly@gmail.com>
2022-09-03 22:39:45 +0300

    решение задачи 02 04 06 08

---
+++ task04/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+        max = num1;
+    }
+    else
+    {
+        max = num3;
+    }
+}
+else
+{
+    if (num2 > num3)
+    {
+        max = num2;
+    }
+    else
+    {
+        max = num3;
+    }
+}
+Console.WriteLine($"{max} - данное число больше других");
+
---
+++ task08/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (num <= 2)
+{
+    Console.WriteLine("Введите другое число ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+while (count <= num)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count}, ");
+        count = count + 2;
+    }
+    else if (count % 2 == 1)
+    {
+        count = count + 1;
+    }
+}
