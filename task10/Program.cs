ec3bbf7b523fa891fc5aac3d03ed0d90a5e33a28
ilya <tojlctbly@gmail.com>
2022-09-09 23:02:53 +0300

    решение  дз-2 задача  13

--- task10/Program.cs
+++ task10/Program.cs
@@ -7,12 +7,14 @@
 Console.Write("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num > 99 && num < 1000)
-{
-    int num2 = (num % 100 - (num % 10)) / 10;
-    Console.WriteLine($"вторая цифра от {num} имеет значение {num2}");
-}
-else
-{
-    Console.WriteLine("Вы ввели не трехзначное число");
-}
+while (num > 99 && num < 1000)
+
+
+// {
+//     int num2 = (num % 100 - (num % 10)) / 10;
+//     Console.WriteLine($"вторая цифра от {num} имеет значение {num2}");
+// }
+// else
+// {
+//     Console.WriteLine("Вы ввели не трехзначное число");
+// }
