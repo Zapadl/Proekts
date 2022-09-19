c1b8e1779ba25d43191726e46ebbaa49c116690d
ilya <tojlctbly@gmail.com>
2022-09-19 22:14:35 +0300

    семинар 5- домашка 34 36 38

---
+++ Task36/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+Console.WriteLine();
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int GetSumPosOddInd(int[] array)
+{
+    int sumPos = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sumPos += array[i];
+    }
+    return sumPos;
+}
+int[] arr = CreateArrayRndInt(7, 1, 10);
+PrintArray(arr);
+int result = GetSumPosOddInd(arr);
+Console.WriteLine();
+Console.WriteLine($"{result}");
---
+++ Task38/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Clear();
+Console.WriteLine();
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max) + rnd.NextDouble();
+        array[i] = Math.Round(array[i], 1);
+    }
+    return array;
+}
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+double Difference(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    int i = 0;
+    while (i < array.Length)
+    {
+        if (max < array[i]) max = array[i];
+        if (min > array[i]) min = array[i];
+        i += 1;
+    }
+    return Math.Round((max - min), 1);
+}
+double[] arr = CreateArrayRndDouble(5, 1, 100);
+PrintArray(arr);
+double result = Difference(arr);
+Console.WriteLine();
+Console.WriteLine($"{result}");
