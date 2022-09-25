bdd82def93f77e7dbf9be3fc4f918a790d56aeef
ilya <tojlctbly@gmail.com>
2022-09-25 15:31:27 +0300

    решение задачи 41 43

---
+++ Task41/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 41. Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}]");
+    }
+}
+
+int[] IntArrayManualMode(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1}е число: ");
+        while (!int.TryParse(Console.ReadLine(), out array[i]))
+        {
+            Console.Write($"Ошибка ввода. \nВведите {i + 1}е число: ");
+        }
+    }
+    return array;
+}
+
+int AboveZero(int[] array)
+{
+    int count = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+
+
+Console.Clear();
+
+int arraySize = default;
+int result = default;
+
+Console.Write("Сколько чисел будем вводить? ");
+while (!int.TryParse(Console.ReadLine(), out arraySize) || arraySize < 0)
+{
+    Console.Write("Ошибка ввода. \nСколько чисел будем вводить? ");
+}
+
+int[] newArray = IntArrayManualMode(arraySize);
+PrintArray(newArray);
+result = AboveZero(newArray);
+Console.Write($" -> {result} совпадений с числами больше ноля.");
+
+
---
+++ Task43/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Метод введения нужных значений вручную с проверкой ввода.
+void ValueManualMode(out double b1_c, out double k1_a, out double b2_d, out double k2_b)
+{
+    Console.WriteLine("Введите необходимые значения для прямых:");
+    // y = k1 * x + b1, y = k2 * x + b2
+    Console.WriteLine("y = ax + c, y = bx + d");
+
+    Console.Write("Введите k1:");
+    while (!double.TryParse(Console.ReadLine(), out k1_a))
+    {
+        Console.Write("Ошибка ввода. Введите k1:");
+    }
+
+    Console.Write("Введите b1:");
+    while (!double.TryParse(Console.ReadLine(), out b1_c))
+    {
+        Console.Write("Ошибка ввода. Введите b1:");
+    }
+
+    Console.Write("Введите k2:");
+    while (!double.TryParse(Console.ReadLine(), out k2_b))
+    {
+        Console.Write("Ошибка ввода. Введите k2:");
+    }
+
+    Console.Write("Введите b2:");
+    while (!double.TryParse(Console.ReadLine(), out b2_d))
+    {
+        Console.Write("Ошибка ввода. Введите b2:");
+    }
+}
+// Метод рассчёта точек пересечения двух прямых.
+void ParallelChecker(double b1_c, double k1_a, double b2_d, double k2_b, double x1, double y1)
+{
+    Console.WriteLine($"y = {k1_a}x + {b1_c}, y = {k2_b}x + {b2_d}");
+    if (k1_a == k2_b && b1_c == b2_d)
+    {
+        Console.WriteLine("Прямые совпадают, у них бесконечное число точек пересечения.");
+    }
+
+    else if (k1_a == k2_b && b1_c != b2_d)
+    {
+        Console.WriteLine("Прямые не имеют точек пересечения, они параллельны.");
+    }
+
+    else
+    {
+        x1 = (b1_c - b2_d) / (k2_b - k1_a);
+        y1 = k2_b * x1 + b2_d;
+        Console.WriteLine($"Точка пересечения двух прямых XY({Math.Round(x1, 2)} ; {Math.Round(y1, 2)}).");
+    }
+}
+
+
+
+Console.Clear();
+
+double b1 = default, k1 = default,
+       b2 = default, k2 = default,
+       x = default, y = default;
+
+ValueManualMode(out b1, out k1, out b2, out k2);
+ParallelChecker(b1, k1, b2, k2, x, y);
+
